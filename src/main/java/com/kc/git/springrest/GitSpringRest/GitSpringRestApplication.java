a80c89ade0aee07f00b4c8bcb7b1747d55fcdc49
kcpalmetto <krishna.palmetto@gmail.com>
2022-12-14 14:13:32 -0500

    first commit

---
+++ src/main/java/com/kc/git/springrest/GitSpringRest/GitSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.kc.git.springrest.GitSpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kc/git/springrest/GitSpringRest/GitSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kc.git.springrest.GitSpringRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
