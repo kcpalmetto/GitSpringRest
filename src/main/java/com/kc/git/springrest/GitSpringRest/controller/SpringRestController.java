6d74229ba9fe358c52e71f1bf5d7d0bd3f7e0585
kcpalmetto <krishna.palmetto@gmail.com>
2022-12-14 14:46:18 -0500

    created rest controller

---
+++ src/main/java/com/kc/git/springrest/GitSpringRest/controller/SpringRestController.java
@@ -0,0 +1,18 @@
+package com.kc.git.springrest.GitSpringRest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class SpringRestController {
+
+	
+	@GetMapping("/hello/{param}")
+	public String hello(@PathVariable String param) {
+		
+		return "Hello " +param;
+	}
+}
